e44c65f95194e304a7f561ef4b45444bd0792a7c
ShivaFisclouds <shiva.maurya@fisclouds.com>
2023-12-01 18:18:34 +0530

    fix: fixes issue with head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,20 @@
-import { Html,Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-
-  return (
-    <Html lang="en">
-      <Head>
-        <title>Saudi Electricity</title>
-      </Head>
-      <body suppressHydrationWarning={true}>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Saudi Electricity</title>
+          {/* Other meta tags, links, etc. */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
